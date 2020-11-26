0e4ee905fb7054a82e545ef6a166b33595479beb
Polina Mrachkovskaya <polina.mrachkovskaya@kisters.de>
2020-11-27 00:54:23 +0100

    mvn build

--- JobLib.groovy
+++ JobLib.groovy
@@ -1,42 +1,53 @@
 def be='https://github.com/kokolanako/be.git'
 def deploy='https://github.com/kokolanako/deploy-template.git'
 
-//job("MS1-checkout") {
-//    label 'build-slave-maven'
-//    scm {
-//        git {
-//            remote {
-//                url(be)
-//            }
-//            branch('ms1')
-//        }
-//
-//    }
-//    steps {
-//        shell('ls -la')
-//    }
-//}
-pipelineJob("PipelineJob-test"){
-    environmentVariables {
-        // these vars could be specified by parameters of this job
-        env('job', 'ms1')
-        env('image', '705249/lol:48') //comma separated string
-        env('container', 'ms1')
-    }
-    definition {
-//        cpsScm {
-//            scm {
-//                git {
-//                    remote {
-//                        url(deploy)
-//                    }
-//                    branch("*/jobDSL")
-//                }
-//            }
-//            scriptPath("Jenkinsfile")
-//        }
-        cps{
-            script(readFileFromWorkspace("JobLib.groovy"))
+job("MS1-checkout") {
+    label 'build-slave-maven'
+    scm {
+        git {
+            remote {
+                url(be)
+            }
+            branch('ms1')
         }
+
+    }
+    steps {
+        shell('ls -la')
+        shell('mvn clean package')
+        shell('ls -la')
     }
 }
+
+
+
+
+
+
+
+
+
+//pipelineJob("PipelineJob-test"){
+//    environmentVariables {
+//        // these vars could be specified by parameters of this job
+//        env('job', 'ms1')
+//        env('image', '705249/lol:48') //comma separated string
+//        env('container', 'ms1')
+//    }
+//    definition {
+////        cpsScm {
+////            scm {
+////                git {
+////                    remote {
+////                        url(deploy)
+////                    }
+////                    branch("*/jobDSL")
+////                }
+////            }
+////            scriptPath("Jenkinsfile")
+////        }
+//        cps{
+//            script(readFileFromWorkspace("Jenkinsfile"))
+//        }
+//    }
+//}
