f41486114c187c84cc4604f486453d6557f4bfb6
Polina Mrachkovskaya <polina.mrachkovskaya@kisters.de>
2020-11-26 13:31:04 +0100

    changed node names

--- JobLib.groovy
+++ JobLib.groovy
@@ -24,7 +24,7 @@ pipelineJob("PipelineJob-test"){
                     remote {
                         url(deploy)
                     }
-                    branch("jobDSL")
+                    branch("*/jobDSL")
                 }
             }
             scriptPath("Jenkinsfile")
