aea173b2b035bdac5efcbf70484b66a795e68a15
Polina Mrachkovskaya <polina.mrachkovskaya@kisters.de>
2020-11-27 11:46:16 +0100

    docker image

--- JobLib.groovy
+++ JobLib.groovy
@@ -1,7 +1,8 @@
 def be='https://github.com/kokolanako/be.git'
 def deploy='https://github.com/kokolanako/deploy-template.git'
 def l1='build-slave-maven'
-def l2='en-jenkins-l-2'
+def d1='en-compile-stage-docker'
+def ms1='ms1'
 
 job("MS1-MVN-BUILD") {
     label l1
@@ -27,8 +28,41 @@ job("MS1-MVN-BUILD") {
     publishers{
         archiveArtifacts {
             pattern('**/target/**SNAPSHOT.jar')
+            pattern('Dockerfile')
             onlyIfSuccessful()
         }
+        downstreamParameterized {
+            trigger('ms1-docker-commit-test') {
+                parameters {
+                    predefinedProp('name', ms1)
+
+                }
+            }
+        }
+    }
+}
+environment {
+    registry = "705249/lol"
+    image = "${env.registry}:${BUILD_NUMBER}"
+    registryCredential = 'dockerhub'
+}
+
+job('ms1-docker-commit-test'){
+    label d1
+    environmentVariables(registry: '705249/lol', image: "${env.registry}:${BUILD_NUMBER}",registryCredential = 'dockerhub')
+    wrappers{
+        credentialBinding{
+            usernamePassword('user','pw','dockerhub')
+        }
+    }
+    steps{
+        copyArtifacts("MS1-MVN-BUILD"){
+
+        }
+        shell('ls -la ')
+        shell("echo ${env.image}")
+        shell('docker build . -t'+${env.image})
+        shell('docker images')
     }
 }
 
