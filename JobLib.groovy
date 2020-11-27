fd5739bd47e8eca5846633057fac1367d4bf9d40
Polina Mrachkovskaya <polina.mrachkovskaya@kisters.de>
2020-11-27 02:51:38 +0100

    mvn

--- JobLib.groovy
+++ JobLib.groovy
@@ -5,7 +5,7 @@ def l2='en-jenkins-l-2'
 
 job("MS1-MVN-BUILD") {
     label l1
-    customWorkspace ("workspace/${JOB_NAME}/${BUILD_NUMBER}")
+//    customWorkspace ("workspace/${JOB_NAME}/${BUILD_NUMBER}")
     scm {
         git {
             remote {
